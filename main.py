9ddda92c8718747e0e9c6193b917dff30175ea72
SoftVibes <chickenpakale@gmail.com>
2024-02-26 17:58:16 +0530

    Finalised the explorer section. Added notif msg feature. Began work on the editor section.

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ import load_settings
 colors = load_settings.fetch_color_profiles()
 
 #Constants (not exactly but yea)
-import load_directory
+import manage_directory
 mode = 'explorer'   #May be explorer or editor depending on which one is being used
-notif_msg = ''   #Msg to be displayed once a major event takes place
-cwd = load_directory.fetch_cwd()   #Fetch the current working directory
+notif_msg = 'Press CTRL+H for help'   #Msg to be displayed once a major event takes place
+cwd = manage_directory.fetch_cwd()   #Fetch the current working directory
+
+
 
 #Function to handle keypresses in the explorer
 def handle_explorer_events(key):
@@ -24,22 +26,31 @@ def handle_explorer_events(key):
         cwd.scroll({curses.KEY_UP: -1, curses.KEY_DOWN: 1}[key])
 
     elif key == curses.KEY_ENTER or key == 13 or key == 10:
-        load_directory.cd(cwd.items[cwd.pos].name)
-        cwd = load_directory.fetch_cwd()
+        global notif_msg
+        if cwd.items[cwd.pos].type == 'dir':
+            manage_directory.cd(cwd.items[cwd.pos].name)
+            cwd = manage_directory.fetch_cwd()
+            notif_msg = 'Changed the current directory. Press CTRL+H for help'
+        elif cwd.items[cwd.pos].type == 'file':
+            try:
+                with open(cwd.items[cwd.pos].name, 'r+') as f:
+                    f.write(f.read())
+                
+            except:
+                pass
+        else:
+            notif_msg = 'Not a file/directory, cannot perform any action on it. Press CTRL+H for help'
 
-
-
-#Function to handle keypresses in the editor
-def handle_editor_events(key):
-    pass
+    elif key == 14:
+        pass
 
 #Function to render the explorer
 def render_explorer():
     y,x = stdscr.getmaxyx()
     top = 0
-    while top + y - 1 < cwd.pos + 1:
-        top += y - 1
-    bottom = y - 1 if len(cwd.items[top:]) > y - 1 else len(cwd.items[top:])
+    while top + y - 2 < cwd.pos + 1:
+        top += y - 2
+    bottom = y - 2 if len(cwd.items[top:]) > y - 2 else len(cwd.items[top:])
 
     stdscr.clear()
     stdscr.refresh()
@@ -53,18 +64,26 @@ def render_explorer():
             c = colors.explorer_file
         elif cwd.items[i].type == 'unknown':
             c = colors.explorer_unknown
-        
+
         c = c[{False: 0, True: 1}[cwd.pos == i]]
         stdscr.addstr(i - top + 1, 0, cwd.items[i].name[:x], c)
 
+    global notif_msg
+    stdscr.insstr(y - 1, 0, (notif_msg + ' '*(x - len(notif_msg) if x > len(notif_msg) else 0))[:x], colors.general[1])
     stdscr.refresh()
+    notif_msg = 'Press CTRL+H for help'
 
+
+
+#Function to handle keypresses in the editor
+def handle_editor_events(key):
+    pass
 
 #Function to render the editor
 def render_editor():
     pass
 
-#Main event loop
+#Main event loopk
 while True:
     if mode == 'explorer':
         render_explorer()
